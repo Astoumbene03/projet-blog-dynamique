7c24bac13e4fa95a64459984d113ae88d61c0450
Astoumbene03 <astoumbenediop2003@gmail.com>
2023-11-12 11:43:09 +0100

    Amelioration dark mode

--- js/script.js
+++ js/script.js
@@ -1,12 +1,18 @@
 var element = document.getElementById("commentaires");
 
+function commenter() {
+  var commentaire = document.getElementById("message-text");
+  var enfant = document.createElement("div");
+  enfant.innerHTML = commentaire.value;
+  element.appendChild(enfant);
+  commentaire.value = "";
 
-function commenter(){
-    var commentaire = document.getElementById("message-text");
-    var enfant = document.createElement("div");
-    enfant.innerHTML=commentaire.value;
-element.appendChild(enfant);
-commentaire.value ="";
+  return false;
+}
 
-return false;
+function mode() {
+  var body = document.getElementsByTagName("html");
+  if (body[0].getAttribute("data-bs-theme") == "light")
+    body[0].setAttribute("data-bs-theme", "dark");
+  else body[0].setAttribute("data-bs-theme", "light");
 }
